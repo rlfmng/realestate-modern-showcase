abba260de64e164c955a772a95fe4aa1bbdf5f21
rlfmng <ralfangelo7215@gmail.com>
2025-08-17 18:01:45 +0800

    sana oks na

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/realestate-modern-showcase/',
   plugins: [react()],
-  base: 'https://rlfmng.github.io/realestate-modern-showcase/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-})
+});
