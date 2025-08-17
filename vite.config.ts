39cf54c1395bfd63dc43db6a55a136ea579de215
rlfmng <ralfangelo7215@gmail.com>
2025-08-17 18:17:29 +0800

    sana ok na

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  base: '/realestate-modern-showcase/',
+  base: '/realestate-modern-showcase/', 
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, 'src'), 
     },
   },
-})
+  build: {
+    outDir: 'dist', 
+  },
+});
